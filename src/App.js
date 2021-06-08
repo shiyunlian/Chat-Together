96333f44f9249975f5633b1d1e1702b412c2d454
shiyunlian <shiyunlian00@gmail.com>
2021-06-08 00:33:31 -0700

    modify ChatEngine props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="9fdef3ba-7b7d-40c2-8efa-179e7dc746ba"
-      userName="Elsa" // username from chatengine.io
-      userSecret="elsa"
+      projectID="9fdef3ba-7b7d-40c2-8efa-179e7dc746ba" // public API key from chatengine.io project
+      userName={localStorage.getItem("username")} // username of a person from chatengine.io project
+      userSecret={localStorage.getItem("password")} // a secret for this person and use it to authenticate
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
